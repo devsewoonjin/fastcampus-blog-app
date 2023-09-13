c40997e416ea664c0be4270dc2a39efff26ba037
devsewoonjin <dev.sewoonjin@gmail.com>
2023-09-14 01:16:47 +0900

    feature/ui publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,55 @@
 import "./App.css";
 import { Route, Routes, Navigate, Link } from "react-router-dom";
+import Home from "./pages/home";
+import PostList from "./pages/posts";
+import PostDetail from "./pages/posts/detail";
+import PostEdit from "./pages/posts/edit";
+import PostNew from "./pages/posts/new";
+import Profile from "./pages/profile";
+import ProfilePage from "./pages/profile";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
+import PostPage from "./pages/posts/detail";
+import PostsPage from "./pages/posts";
 
 function App() {
   return (
     <>
-      <ul>
+      {/* <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Home Page</Link>
         </li>
         <li>
-          <Link to="/posts">Post List</Link>
+          <Link to="/posts">Post List Page</Link>
         </li>
         <li>
-          <Link to="/posts:id">Post Detail</Link>
+          <Link to="/posts:id">Post Detail Page</Link>
         </li>
         <li>
-          <Link to="/posts/new">Post New</Link>
+          <Link to="/posts/new">Post New Page</Link>
         </li>
         <li>
-          <Link to="/posts/edit">Post Edit</Link>
+          <Link to="/posts/edit">Post Edit Page</Link>
         </li>
         <li>
-          <Link to="/profile">Profile Edit</Link>
+          <Link to="/profile">Profile Page</Link>
         </li>
-      </ul>
+        <li>
+          <Link to="/login">Login Page</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup Page</Link>
+        </li>
+      </ul> */}
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/posts" element={<div>Post List Page</div>} />
-        <Route path="/posts:id" element={<div>Post Detail Page</div>} />
-        <Route path="/posts/new" element={<div>Post New Page</div>} />
-        <Route path="/posts/edit" element={<div>Post Edit Page</div>} />
-        <Route path="/profile" element={<div>Profile Page</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="/posts/new" element={<PostNew />} />
+        <Route path="/posts/edit" element={<PostEdit />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </>
---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,39 @@
+export default function PostDetail() {
+  return (
+    <div className="post__detail">
+      <div className="post__box">
+        <div className="post__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+        <div className="post__profile-box">
+          <div className="post__profile" />
+          <div className="post__author-name">패스트캠퍼스</div>
+          <div className="post__date">2023.0708 토요일</div>
+        </div>
+        <div className="post__utils-box">
+          <div className="post__delete">삭제</div>
+          <div className="post__eldit">수정</div>
+        </div>
+        <div className="post__text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam at
+          dolore, optio repellat voluptatum qui nulla, architecto totam commodi
+          ex quod quo suscipit maxime necessitatibus veritatis reiciendis ad
+          quibusdam molestias! Deserunt doloremque fuga corrupti! Cupiditate
+          itaque, ab maiores omnis tempore blanditiis eum pariatur esse impedit
+          dicta magni vel tempora nulla non praesentium odio illo doloribus
+          dignissimos. Dolorem exercitationem alias neque. Qui nihil suscipit
+          accusamus unde placeat? Fugiat optio, quos pariatur eaque eum quod
+          facilis dolorem iusto asperiores ut doloribus necessitatibus cumque
+          sunt. Hic natus tenetur blanditiis delectus corporis eveniet! Neque.
+          Ut ipsam, harum laboriosam tempora sequi quia, iure iste error itaque
+          delectus veniam magnam incidunt molestias suscipit doloribus in ipsum
+          quidem! Ullam ut odio soluta excepturi, facere ducimus suscipit
+          numquam. Officia, rerum. Expedita veniam provident, adipisci nam ea
+          quis similique beatae aliquam cum. Veritatis reprehenderit eos ratione
+          dolore expedita quam perferendis quasi? Eum eligendi, tempora fugiat
+          magnam fugit deleniti perferendis!
+        </div>
+      </div>
+    </div>
+  );
+}
